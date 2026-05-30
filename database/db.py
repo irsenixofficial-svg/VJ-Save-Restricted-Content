42c41047492589e0b75ed0c33997d7dec5fa4732
irsenixofficial-svg <irsenixofficial@gmail.com>
2026-05-30 18:16:23 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit 𝙄𝙍𝙎𝙀𝙉𝙄𝙓
+# Subscribe YouTube Channel For Amazing Bot
+# Ask Doubt on telegram
 
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
