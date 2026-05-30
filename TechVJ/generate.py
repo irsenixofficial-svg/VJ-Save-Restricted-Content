e3a490788014b4b903cac69ff9e77c3dce8d74f4
irsenixofficial-svg <irsenixofficial@gmail.com>
2026-05-30 18:20:20 +0530

    Update generate.py

--- TechVJ/generate.py
+++ TechVJ/generate.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - 𝙄𝙍𝙎𝙀𝙉𝙄𝙓
+# Subscribe YouTube Channel For Amazing bot
+# Ask Doubt on telegram
 
 import traceback
 from pyrogram.types import Message
